cee44a72517d2b92be3e8c2d2520f9f1fc1d914b
hqt1642002 <huynhquoctoan1642002@gmail.com>
2022-12-24 09:20:06 +0700

    first commit

---
+++ js/index/script.js
@@ -0,0 +1,9 @@
+﻿const gift = document.querySelector('.gift');
+const fourleaf = document.querySelector('.fourleaf');
+const message = document.querySelector('.message');
+
+gift.addEventListener('click', () => {
+    gift.classList.toggle('is-open');
+    fourleaf.classList.toggle('is-fade-in');
+    message.classList.toggle('is-visible');
+});
